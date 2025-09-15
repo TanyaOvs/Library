8a0e0468f5a77bddcd1a7ca9f88d91f1286a172a
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-09-15 21:26:47 +0300

    Created static methods for validating Book class

--- Book.cs
+++ Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 
 namespace Library
 {
@@ -13,41 +14,91 @@ namespace Library
         public string Title
         {
             get { return title; }
-            set { title = value; }
+            set { ValidateTitle(value); title = value; }
         }
 
         public string Author
         {
             get { return author; }
-            set { author = value; }
+            set { ValidateAuthor(value); author = value; }
         }
 
         public string Genre
         {
             get { return genre; }
-            set { genre = value; }
+            set { ValidateGenre(value); genre = value; }
         }
 
         public double CollateralValue
         {
             get { return collateralValue; }
-            set { collateralValue = value; }
+            set { ValidateCollateralValue(value); collateralValue = value; }
         }
 
         public double RentalCost
         {
             get { return rentalCost; }
-            set { rentalCost = value; }
+            set { ValidateRentalCost(value); rentalCost = value; }
         }
 
-        public Book(string _title, string _author, string _genre, double _collateral_value, double _rental_cost) 
+        public Book(string title, string author, string genre, double collateral_value, double rental_cost) 
         {
-            Title = _title;
-            Author = _author;
-            Genre = _genre;
-            CollateralValue = _collateral_value;
-            RentalCost = _rental_cost;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            CollateralValue = collateral_value;
+            RentalCost = rental_cost;
         }
 
+        private static void ValidateTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Название книги не может быть пустым или состоять только из пробелов!");
+
+            if (!Regex.IsMatch(title, @"^[\d\w\s,:!?а-яА-ЯёЁ-]+$"))
+                throw new ArgumentException("Название книги содержит запрещенные символы!");
+        }
+
+        private static void ValidateAuthor(string author)
+        {
+            if (string.IsNullOrWhiteSpace(author))
+                throw new ArgumentException("ФИО автора не может быть пустым или состоять только из пробелов!");
+
+            if (!Regex.IsMatch(author, @"^[А-ЯЁ][а-яё]+(?:[\- ][А-ЯЁ]?[а-яё]+)*$"))
+                throw new ArgumentException("ФИО автора должно содержать только русские буквы, начинаться с заглавной буквы, и может содержать пробелы или тире!");
+        }
+
+        private static void ValidateGenre(string genre)
+        {
+            if (string.IsNullOrWhiteSpace(genre))
+                throw new ArgumentException("Жанр не может быть пустым или состоять только из пробелов!");
+
+            if (!Regex.IsMatch(genre, @"^[А-ЯЁ][а-яё]+(?:[\- ][А-ЯЁ]?[а-яё]+)*$"))
+                throw new ArgumentException("Жанр должен содержать только русские буквы, начинаться с заглавной буквы, и может содержать пробелы или тире!");
+        }
+
+        private static void ValidateCollateralValue(double сollateralValue)
+        {
+            if (сollateralValue < 0)
+                throw new ArgumentException("Залоговая стоимость не может быть отрицательной!");
+
+            if (сollateralValue == 0)
+                throw new ArgumentException("Залоговая стоимость не может быть нулевой!");
+
+            if (сollateralValue > 10000)
+                throw new ArgumentException("Залоговая стоимость слишком высока!");
+        }
+
+        private static void ValidateRentalCost(double rentallValue)
+        {
+            if (rentallValue < 0)
+                throw new ArgumentException("Стоимость проката не может быть отрицательной!");
+
+            if (rentallValue == 0)
+                throw new ArgumentException("Стоимость проката не может быть нулевой!");
+
+            if (rentallValue > 10000)
+                throw new ArgumentException("Стоимость проката слишком высока!");
+        }
     }
 }
