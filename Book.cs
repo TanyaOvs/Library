22610bdf9048791b55544f696abe0464c7e76da9
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-09-20 14:35:22 +0300

    Update Book.cs
    Добавлены два новых поля (свойства и валидация для них): book_id и isbn.
    Реализована перегрузка конструктора для типов: строка, JSON.

--- Book.cs
+++ Book.cs
@@ -1,15 +1,32 @@
 using System;
+using System.IO;
+using System.Text.Json;
 using System.Text.RegularExpressions;
+
 
 namespace Library
 {
     public class Book
     {
+        private int book_id;
+        private string isbn;
         private string title;
         private string author;
         private string genre;
         private double collateralValue;
         private double rentalCost;
+
+        public int Book_ID
+        {
+            get { return book_id; }
+            set { ValidateBookID(value);book_id = value; }
+        }
+
+        public string ISBN
+        {
+            get { return isbn; }
+            set { ValidateISBN(value); isbn = value; }
+        }
 
         public string Title
         {
@@ -41,13 +58,92 @@ namespace Library
             set { ValidatePrice(value, "Стоимость проката"); rentalCost = value; }
         }
 
-        public Book(string title, string author, string genre, double collateralValue, double rentalCost) 
+        public Book(int book_id, string isbn, string title, string author, string genre, double collateralValue, double rentalCost) 
         {
+            Book_ID = book_id;
+            ISBN = isbn;
             Title = title;
             Author = author;
             Genre = genre;
             CollateralValue = collateralValue;
             RentalCost = rentalCost;
+        }
+
+        public Book(string dataString)
+        {
+            var data_parts = dataString.Split(';');
+            if (data_parts.Length != 7)
+                throw new ArgumentException("Строка должна содержать 7 полей, разделенных ';'");
+
+            Book_ID = int.Parse(data_parts[0]);
+            ISBN = data_parts[1];
+            Title = data_parts[2];
+            Author = data_parts[3];
+            Genre = data_parts[4];
+            CollateralValue = double.Parse(data_parts[5]);
+            RentalCost = double.Parse(data_parts[6]);
+        }
+
+
+        public Book(string filePath, bool fromFile)
+        {
+            //string absolutePath = Path.GetFullPath(filePath);
+            ////Console.WriteLine($"Ищем файл по абсолютному пути: {absolutePath}");
+
+            //if (!File.Exists(absolutePath))
+            //    throw new FileNotFoundException($"Файл не найден: {absolutePath}");
+
+            if (!fromFile)
+                throw new ArgumentException("Для загрузки из JSON файла необходимо указать true в качестве второго параметра для конструктора!");
+
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException("Путь к файлу не может быть пустым.");
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Файл не найден.", filePath);
+
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                using JsonDocument doc = JsonDocument.Parse(jsonString);
+                JsonElement root = doc.RootElement;
+
+                Book_ID = root.GetProperty("book_id").GetInt32();
+                ISBN = root.GetProperty("isbn").GetString();
+                Title = root.GetProperty("title").GetString();
+                Author = root.GetProperty("author").GetString();
+                Genre = root.GetProperty("genre").GetString();
+                CollateralValue = root.GetProperty("collateralValue").GetDouble();
+                RentalCost = root.GetProperty("rentalCost").GetDouble();
+            }
+            catch (JsonException ex)
+            {
+                throw new ArgumentException("Неверный JSON формат в файле: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ошибка чтения файла: " + ex.Message);
+            }
+        }
+
+
+        private static void ValidateBookID(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException("ID книги должен быть положительным числом!");
+
+            if (id > 999999)
+                throw new ArgumentException("ID книги не может превышать 6 цифр!");
+        }
+
+        private static void ValidateISBN(string isbn)
+        {
+            if (string.IsNullOrWhiteSpace(isbn))
+                throw new ArgumentException("ISBN не может быть пустым!");
+
+            // Регулярное выражение для формата ISBN-13: (978|979)-5-12345-678-0
+            if (!Regex.IsMatch(isbn, @"^(978|979)-\d-\d{5}-\d{3}-\d$"))
+                throw new ArgumentException("ISBN должен быть в формате: '978-*-*****-***-*' или '979-*-*****-***-*'.");
         }
 
         private static void ValidateTitle(string title)
