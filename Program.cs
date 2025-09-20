06e27556de0e1910f1847baa8567ad4712899484
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-09-20 14:40:11 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание book на основе ручного ввода
+             Book firstBook = new Book(000001, "979-5-12345-678-0", "1984", "Джордж Оруэлл", "Антиутопия", 500, 800);
+
+            // Создание book на основе строки
+            // Пример книги для ввода: 000001;979-5-12345-678-0;1984;Джордж Оруэлл;Антиутопия;500;800
+            Console.WriteLine("Введите данные в одну строку, разделяя данные знаком ';' в формате:\n'ID_Книги;ISBN;Название;Автор;Жанр;Залоговая_стоимость;Стоимость_проката'.");
+            string input_data = Console.ReadLine().Trim();
+            Book secondBook = new Book(input_data);
+            Console.WriteLine("Экземпляр класса Book был успешно создан! :з");
+
+            // Создание book на основе JSON файла
+            try
+            {
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "book.json");
+                Book thirdbook = new Book(fullPath, true);
+                Console.WriteLine("Книга успешно загружена из файла!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
