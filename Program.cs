37699e12544e2129b8f732b6450176ef53fc0e2d
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-09-26 16:57:50 +0300

    Update Program.cs
    Добавлены новые примеры.

--- Program.cs
+++ Program.cs
@@ -7,13 +7,20 @@ namespace Library
     {
         static void Main(string[] args)
         {
-            // Создание book на основе ручного ввода
-             Book firstBook = new Book(000001, "979-5-12345-678-0", "1984", "Джордж Оруэлл", "Антиутопия", 500, 800);
+            // Создание Book на основе ручного ввода
+             Book firstBook = new Book(1, "979-5-12345-678-0", "Евгений Онегин", "Александр Сергеевич Пушкин", "Роман", 500, 800);
+             firstBook.PrintFullInfo();
+             firstBook.PrintShortInfo();
+
+            // Создание BookPreview
+            BookPreview bp = new BookPreview(1, "979-5-12345-678-0", "Евгений Онегин", "Александр Сергеевич Пушкин", "Роман", 500, 800);
+            bp.PrintFullInfo();
+            bp.PrintShortInfo();
 
             // Создание book на основе строки
-            // Пример книги для ввода: 000001;979-5-12345-678-0;1984;Джордж Оруэлл;Антиутопия;500;800
+            // Пример книги для ввода: 1;979-5-12345-678-0;1984;Джордж Оруэлл;Антиутопия;500;800
             Console.WriteLine("Введите данные в одну строку, разделяя данные знаком ';' в формате:\n'ID_Книги;ISBN;Название;Автор;Жанр;Залоговая_стоимость;Стоимость_проката'.");
-            string input_data = Console.ReadLine().Trim();
+            string input_data = Console.ReadLine();
             Book secondBook = new Book(input_data);
             Console.WriteLine("Экземпляр класса Book был успешно создан! :з");
 
