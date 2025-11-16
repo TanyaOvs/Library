57418d46cb698914b4e5b9fd424fd4b5a283d29a
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-11-16 19:11:22 +0300

    2) Book_rep_yaml: пункт "h. Удалить элемент списка по ID"

--- Book_rep_yaml.cs
+++ Book_rep_yaml.cs
@@ -258,6 +258,12 @@ namespace Library
             }
             return bookList;
         }
+
+        public List<Book> DeleteBookInList(List<Book> bookList, Book book)
+        {
+            bookList.Remove(book);
+            return bookList;
+        }
         
     }
 }
