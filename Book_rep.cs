e76e679293ded826114001263573265a0004a74e
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-11-26 19:58:13 +0300

    3) Book_rep: класс, объединяющий Book_rep_json и Book_rep_yaml в одну иерархию

---
+++ Book_rep.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Library
+{
+    public class Book_rep
+    {
+        public Book_rep() { }
+        public Book GetBookByID(List<Book> bookList, int bookID)
+        {
+            if (bookList == null || bookList.Count == 0)
+                throw new ArgumentException("Список книг пуст или не инициализирован.");
+
+            // Ищем книгу с помощью метода LINQ, который возвращает первый элемент, удовлетворяющий условию с bookID
+            Book foundBook = bookList.FirstOrDefault(book => book.BookID == bookID);
+
+            if (foundBook == null)
+                throw new ArgumentException($"Книга с ID={bookID} не найдена в списке.");
+
+            return foundBook;
+        }
+        public void PrintBooksList<T>(List<T> bookList) where T : Book
+        {
+            if (bookList == null || bookList.Count == 0)
+            {
+                Console.WriteLine("Список книг пуст.");
+                return;
+            }
+
+            string separator = "-------------------------";
+            Console.WriteLine(separator);
+            Console.WriteLine("Список книг:");
+
+            foreach (var book in bookList)
+            {
+                Console.WriteLine(separator);
+                book.PrintFullInfo();
+            }
+
+            Console.WriteLine(separator);
+        }
+
+        public void Get_K_N_ShortList(List<BookPreview> k, int n)
+        {
+            const int pageSize = 2; // Количество объектов на одной "странице"
+
+            if (k == null || k.Count == 0)
+                throw new ArgumentException("Список книг пуст или не инициализирован!");
+
+            if (n <= 0)
+                throw new ArgumentException("Номер страницы должен быть положительным числом!");
+
+            // Вычисляем, сколько элементов нужно пропустить, чтобы начать с нужной "страницы"
+            int skipCount = (n - 1) * pageSize;
+
+            if (skipCount >= k.Count)
+                throw new ArgumentException("Номер страницы выходит за пределы списка книг!");
+
+            List<BookPreview> result = k.Skip(skipCount).Take(pageSize).ToList();
+            PrintBooksList(result);
+        }
+
+        private bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return !string.IsNullOrEmpty(str);
+        }
+
+        public void SortBooksByTitle(List<Book> bookList)
+        {
+            if (bookList == null || bookList.Count == 0)
+            {
+                Console.WriteLine("Список пуст или не существует.");
+                return;
+            }
+
+            for (int i = 0; i < bookList.Count - 1; i++)
+            {
+                for (int j = i + 1; j < bookList.Count; j++)
+                {
+                    Book book1 = bookList[i];
+                    Book book2 = bookList[j];
+
+                    bool isDigits1 = IsDigitsOnly(book1.Title);
+                    bool isDigits2 = IsDigitsOnly(book2.Title);
+
+                    // Если обе книги содержат только цифры в названии, то сортируем как числа
+                    if (isDigits1 && isDigits2)
+                    {
+                        long num1 = long.Parse(book1.Title);
+                        long num2 = long.Parse(book2.Title);
+
+                        if (num1 > num2)
+                            (bookList[i], bookList[j]) = (bookList[j], bookList[i]);
+                    }
+
+                    // Если название первой книги содержит только цифры, то она идет раньше в списке
+                    else if (isDigits1 && !isDigits2)
+                        (bookList[i], bookList[j]) = (bookList[j], bookList[i]);
+
+                    // Если название второй книги содержит только цифры, то она идёт позже в списке
+                    else if (!isDigits1 && isDigits2)
+                        continue;
+
+                    // Если названия обеих книг строковые, то выполняем обычную лексикографическую сортировку
+                    else if (string.Compare(book1.Title, book2.Title, StringComparison.OrdinalIgnoreCase) > 0)
+                        (bookList[i], bookList[j]) = (bookList[j], bookList[i]);
+                }
+            }
+
+            Console.WriteLine("Список книг успешно отсортирован по названию!");
+            PrintBooksList(bookList);
+        }
+
+        public List<Book> AddBookInList(List<Book> bookList, Book book)
+        {
+            foreach (Book bookElem in bookList)
+            {
+                if (Book.CompareBooks(book, bookElem))
+                {
+                    throw new ArgumentException($"Нельзя добавить книгу {book.Title}: книга с таким же ISBN уже находится в списке!");
+                }
+            }
+            bookList.Add(book);
+            return bookList;
+        }
+
+        public List<Book> ChangeBookByID(List<Book> bookList, Book book, int bookID)
+        {
+            // if (bookID < 0 || bookID >= bookList.Count)
+            if (bookID < 0)
+                throw new ArgumentException("ID выходит за пределы списка bookList!");
+
+            foreach (Book bookElem in bookList)
+            {
+                if (bookElem.BookID == bookID)
+                {
+                    bookList[bookList.IndexOf(bookElem)] = book;
+                    break;
+                }
+            }
+            return bookList;
+        }
+
+        public List<Book> DeleteBookInList(List<Book> bookList, Book book)
+        {
+            bookList.Remove(book);
+            return bookList;
+        }
+
+        public int Get_Count(List<Book> bookList)
+        {
+            return bookList.Count;
+        }
+    }
+}
