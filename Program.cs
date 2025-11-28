d81b280098140af73b1b9296a78b31b7e9fe5bfe
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-11-28 09:16:10 +0300

    4) Program: работа с базой данных

--- Program.cs
+++ Program.cs
@@ -9,100 +9,53 @@ namespace Library
     {
         static void Main(string[] args)
         {
-            // Работа с JSON
-            /*
+            // Работа с базой данных
             try
             {
-                // Чтение объектов класса Book в List на основе JSON файла
-                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "books.json");
-                
-                // 1. Объект для работы с JSON
-                Book_rep_json brj = new Book_rep_json();
+                // Строка для подключения к БД
+                string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=password;Database=Library";
 
-                // a. Создание списка на основе JSON
-                List<Book> bookList = brj.ReadJson(fullPath);
+                // 1. Объект для работы с БД
+                Book_rep_DB brdb = new Book_rep_DB(connectionString);
 
-                // b. Запись данных в файл
-                brj.WriteJson(bookList);
+                // a. Получить объект по ID
+                Book testBook = brdb.GetBookByID(5);
+                testBook.PrintFullInfo();
 
-                // c. Получение объекта по ID
-                Book foundBook = brj.GetBookByID(bookList, 2);
-                Console.WriteLine($"Найденная книга: {foundBook.Title}");
+                // b. Получить список k по счету n объектов класса short 
+                List<BookPreview> bpList = brdb.Get_K_N_ShortList(4, 2);
 
-                // d. Получить список k по счету n объектов класса short
-                List<BookPreview> bookPreviewList = brj.ReadShortJson(fullPath);
-                brj.Get_K_N_ShortList(bookPreviewList, 5);
+                string beautifulSeparator = "-------------";
+                Console.WriteLine(beautifulSeparator);
+                Console.WriteLine("Список книг: ");
 
-                // e. Сортировать элементы по выбранному полю (title)
-                brj.SortBooksByTitle(bookList);
+                foreach (BookPreview bp in bpList)
+                {
+                    Console.WriteLine(beautifulSeparator);
+                    bp.PrintShortInfo();
+                }
+                Console.WriteLine(beautifulSeparator);
 
-                // f. Добавить объект в список
-                Book firstBook = new Book("979-5-54360-678-0", "Одиссея", "Гомер", "Эпос");
-                bookList = brj.AddBookInList(bookList, firstBook);
-                brj.PrintBooksList(bookList);
+                // c. Добавить объект в список
+                Book superBook = new Book("979-5-12363-123-0", "Илиада", "Гомер", "Эпос");
+                //brdb.AddBook(superBook);
 
-                // g. Заменить элемент списка по ID
-                Book secondBook = new Book("979-5-12363-123-0", "Илиада", "Гомер", "Эпос");
-                bookList = brj.ChangeBookByID(bookList, secondBook, 2);
+                // d. Заменить элемент списка по ID
+                Book superDuperBook = new Book("979-5-12993-321-9", "Записки Доктора Ватсона", "Артур Конан Дойл", "Детектив");
+                //brdb.UpdateBook(12, superDuperBook);
 
-                // h. Удалить элемент списка по ID
-                bookList = brj.DeleteBookInList(bookList, firstBook);
+                // e. Удалить элемент списка по ID
+                //brdb.DeleteBook(12);
 
-                // i. Получить количество элементов
-                Console.WriteLine($"Количество элементов в списке bookList: {brj.Get_Count(bookList)}");
+                // f. Получить количество элементов
+                Console.WriteLine($"Количество книг в базе данных: {brdb.Get_Count()}");
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка: {ex.Message}");
             }
-            */
-
-            // Работа с YAML
-            try
-            {
-                // Чтение объектов класса Book в List на основе yaml 
-                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "books.yaml");
-
-                // 1. Объект для работы с yaml
-                Book_rep_yaml bry = new Book_rep_yaml();
-
-                // a. Чтение всех значений из yaml (создание списка книг)
-                List<Book> bookList = bry.ReadYaml(fullPath);
-
-                // b. Запись данных в файл
-                //bry.WriteYaml(bookList);
-
-                // c. Получение объекта по ID
-                Book foundBook = bry.GetBookByID(bookList, 5);
-                Console.WriteLine($"Найденная книга: {foundBook.Title}");
-
-                // d. Получить список k по счету n объектов класса short
-                //List<BookPreview> bookPreviewList = bry.ReadShortYaml(fullPath);
-                //bry.Get_K_N_ShortList(bookPreviewList, 5);
-
-                // e. Сортировать элементы по выбранному полю (title)
-                //bry.SortBooksByTitle(bookList);
-
-                // f. Добавить объект в список
-                Book firstBook = new Book("979-5-54360-678-0", "Одиссея", "Гомер", "Эпос");
-                bookList = bry.AddBookInList(bookList, firstBook);
-                //bry.PrintBooksList(bookList);
-
-                // g. Заменить элемент списка по ID
-                Book secondBook = new Book("979-5-12363-123-0", "Илиада", "Гомер", "Эпос");
-                bookList = bry.ChangeBookByID(bookList, secondBook, 2);
-
-                // h. Удалить элемент списка по ID
-                bookList = bry.DeleteBookInList(bookList, firstBook);
-
-                // i. Получить количество элементов
-                Console.WriteLine($"Количество элементов в списке bookList: {bry.Get_Count(bookList)}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.Message}");
-            }
+            
         }
     }
 }
