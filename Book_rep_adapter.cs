4c0e651f40827a57a49090a534f7f7e98ea12934
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2025-12-04 20:15:25 +0300

    7) Book_rep_adapter: пункт "с помощью паттерна Декоратор реализовать возможность передачи фильтра и способа сортировки в методы get_k_n_short_list и get_count для класса работы с БД"

---
+++ Book_rep_adapter.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace Library
+{
+    public interface IBookRepository
+    {
+        List<BookPreview> Get_K_N_ShortList(int n);
+        int Get_Count();
+    }
+
+    public class Book_rep_adapter : IBookRepository
+    {
+        private readonly Book_rep_DB inner;
+
+        public Book_rep_adapter(Book_rep_DB db)
+        {
+            inner = db;
+        }
+
+        public Book GetBookByID(int id)
+        {
+            return inner.GetBookByID(id);
+        }
+
+        public List<BookPreview> Get_K_N_ShortList(int n)
+        {
+            return inner.Get_K_N_ShortList(n);
+        }
+
+        public int Get_Count()
+        {
+            return inner.Get_Count();
+        }
+    }
+
+}
